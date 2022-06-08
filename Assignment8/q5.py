14b3c70b424e4ea12f2de8aed189b5e661d3c0da
Dhruv Ashish Patil <49668870+DhruvAPat@users.noreply.github.com>
2022-06-08 21:56:18 +0530

    Add files via upload

---
+++ Assignment8/q5.py
@@ -0,0 +1,14 @@
+#5.	Write a Python Program to Remove Punctuation From a String?
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+my_str = "Hello!!!, he said ---and went."
+
+
+
+no_punct = ""
+for char in my_str:
+   if char not in punctuations:
+       no_punct = no_punct + char
+
+print(no_punct)
