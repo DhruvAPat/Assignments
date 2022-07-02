af4df0bc5bcfadb83b200f69d914b6f209c2bca9
Dhruv Ashish Patil <49668870+DhruvAPat@users.noreply.github.com>
2022-07-02 10:45:51 +0530

    Add files via upload

---
+++ Leetcode/ReverseInteger.py
@@ -0,0 +1,19 @@
+#Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+#Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+#Solution
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        s = str(abs(x))[::-1]
+        if x < 0:
+            if len(s) >= 10 and s > '2147483648':
+                return 0
+            return -int(s)
+        else:
+            if len(s) >= 10 and s > '2147483647':
+                return 0
+            return int(s)
+
+ 
